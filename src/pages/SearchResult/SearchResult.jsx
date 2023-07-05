2ae3f5f6fce70f788b61f86f464a652c5ba66cda
ashraful-commits <ashrfulalam10000@gmail.com>
2023-07-05 10:53:49 +0600

    6

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -6,10 +6,10 @@ import "./SearchResult.scss";
 
 import { fetchDataFromApi } from "../../utils/api";
 
-import Spinner from "../../components/spinner/Spinner";
 import noResults from "../../assets/no-results.png";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
+import Spinner from "../../components/spinner/Spinner";
 
 const SearchResult = () => {
   const [data, setData] = useState(null);
