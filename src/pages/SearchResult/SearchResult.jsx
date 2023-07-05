89feff6cb470eca74e968a934e7c04a8a5cd7761
ashraful-commits <ashrfulalam10000@gmail.com>
2023-07-05 10:49:40 +0600

    3

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -5,10 +5,11 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import "./SearchResult.scss";
 
 import { fetchDataFromApi } from "../../utils/api";
-import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
 import noResults from "../../assets/no-results.png";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const SearchResult = () => {
   const [data, setData] = useState(null);
